2d91ff11987f771cc7b5c717b881152bc7eb25ef
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-10-17 13:37:51 +0200

    Update the config.js file with additional env variables

--- app/config.js
+++ app/config.js
@@ -1,3 +1,24 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/just-jotit';
+'use strict';
 
-exports.PORT = process.env.PORT || 8080;
+const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/just-jotit';
+
+const TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost:27017/test-just-jotit';
+
+const PORT = process.env.PORT || 8080;
+
+const HTTP_STATUS_CODES = {
+  OK: 200, 
+  CREATED: 201,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500
+}
+
+const JWT_SECRET = process.env.JWT_SECRET || 'default';
+
+const JWT_EXPIRY = process.env.JWT_EXPIRY || '7d'; 
+
+
+module.exports = {DATABASE_URL, TEST_DATABASE_URL, PORT, HTTP_STATUS_CODES, JWT_EXPIRY, JWT_SECRET};
