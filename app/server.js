8331232c565ac091e1715d6a9be4c03ec9ec0fa7
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-10-17 10:35:16 +0200

    Add middleware

--- app/server.js
+++ app/server.js
@@ -1,10 +1,28 @@
 'use strict';
 
+// Import libraries, frameworks
+
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+// Create an express app instance
 const app = express();
 
+// Declare a variable which will hold the server instance
 let server;
+
+// App Middleware
+// -------------------------
+
+// HTTP request logger
+app.use(morgan('common'));
+
+// Body parser
+app.use(express.json());
+
+// Send static files to client
+app.use(express.static('./public'));
+
+// --------------------------
