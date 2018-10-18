7e26b291710d11a5dd84e1f64f896cef0a6d8d68
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-10-18 11:17:59 +0200

    Implement the note schema and model

--- app/note/note.model.js
+++ app/note/note.model.js
@@ -10,3 +10,15 @@ mongoose.Promise = global.Promise;
 // Define the schema constructor
 const Schema = mongoose.Schema;
 
+// Define the note schema
+const noteSchema = Schema({
+  user: {type: mongoose.Schema.Types.ObjectId, ref: 'user'},
+  title: {type: String, required: true},
+  content: {type: String, required: true},
+  createDate: {type: Date},
+  updateDate: {type: Date, default: Date.now}
+});
+
+const Note = mongoose.model('Note', noteSchema);
+
+module.exports = {Note};
